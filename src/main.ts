d25a17cafa89fc920907e840b6d4e4bac72a092d
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-09 09:42:58 -0300

    feat(main.ts): update boilerplate code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
+import { FormHandler } from "./lib/FormHandler";
+import { TabSwitcher } from "./lib/TabSwitcher";
 import "./styles/style.css";
+import "toastify-js/src/toastify.css";
+
+const tabs = document.getElementById("tabs") as HTMLDivElement;
+const form = document.getElementById("form-tab") as HTMLFormElement;
+const tabSwitcher = new TabSwitcher("form");
+const formHandler = new FormHandler();
+
+tabs.addEventListener("click", (e) => tabSwitcher.switch(e));
+form.addEventListener("submit", (e) => formHandler.handleSubmit(e));
