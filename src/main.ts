899c7ecd7e4c176f21f1c6b015eef6c94e5b0232
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-10 11:12:05 -0300

    refactor(main.ts): remove boilerplate code and use App class

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,4 @@
-import { FormHandler } from "./lib/FormHandler";
-import { TabSwitcher } from "./lib/TabSwitcher";
+import { App } from "./lib/App";
 import "./styles/style.css";
 import "toastify-js/src/toastify.css";
-
-const tabs = document.getElementById("tabs") as HTMLDivElement;
-const form = document.getElementById("form-tab") as HTMLFormElement;
-const tabSwitcher = new TabSwitcher("form");
-const formHandler = new FormHandler();
-
-tabs.addEventListener("click", (e) => tabSwitcher.switch(e));
-form.addEventListener("submit", (e) => formHandler.handleSubmit(e));
+new App().init();
